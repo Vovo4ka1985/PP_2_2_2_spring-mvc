a226b80315dd3584afc4fbd231a4a38e60240538
Vovo4ka1985 <hammett2010@mail.ru>
2022-05-23 19:42:47 +0400

    new1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,28 +1,32 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping ("/cars")
     public class CarController {
-        @GetMapping(value = "/cars")
-        public String printCar(@RequestParam (value = "count", defaultValue = "5") int allCars, Model model) {
-            List<Car>list = new ArrayList<>();
-            list.add(new Car("Lexus", "Red", "V6"));
-            list.add(new Car("BMW", "Blue", "V8"));
-            list.add(new Car("Lada", "Black", "1.6"));
-            list.add(new Car("Audi", "Pink", "V8"));
-            list.add(new Car("Toyota", "Yellow", "V6"));
-            list = CarServiceImpl.carsCount(list, allCars);
-            model.addAttribute("list", list);
+
+    private CarService carService;
+@Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping()
+        public String printCar(@RequestParam (value = "count", defaultValue = "5") int number, Model model) {
+            model.addAttribute("car", carService.index(number));
             return "cars";
         }
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,15 +1,25 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-public class CarServiceImpl {
+@Service
+public class CarServiceImpl implements CarService {
     //Метод , который При запросе /cars выводит весь список. При запросе /cars?count=2 должен отобразиться список из 2 машин,
     //при /cars?count=3 - из 3, и тд. При count ? 5 выводить весь список машин.
-    public static List<Car> carsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
-        return list.stream().limit(number).collect(Collectors.toList());
+    private List<Car> cars = Arrays.asList(new Car("Lexus", "Red", "V6"),
+        new Car("BMW", "Blue", "V8"),
+        new Car("Lada", "Black", "1.6"),
+            new Car("Audi", "Pink", "V8"),
+            new Car("Toyota", "Yellow", "V6"));
+
+    @Override
+    public List<Car> index(int number) {
+        return cars.stream().limit(number).collect(Collectors.toList());
+
     }
 }
